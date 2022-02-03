bcff9831e08fb0632eaa47bc9527288f194af9dc
Betelihem <Betelihem>
2022-02-02 21:34:08 -0800

    commit tree 09b7329f36b6da9912b395cf68de95b6b363e40f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(record) {
+    let isWin = record.find (function(record) {
+        return record.result === 'W'
+    })
+
+    if (isWin) {
+        return isWin.year
+    } else {
+        return
+    }
+}
